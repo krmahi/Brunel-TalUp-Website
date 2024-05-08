c958ab3c51c619b41ed9b87fa12343bf9ad27dac
Mahesh kumar <mahesh.kr.2277@gmail.com>
2024-05-08 20:51:42 +0530

    fix: reactDOM and react structure for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import React from 'react';
+
 import Home from './pages/Home';
 import "./App.css"
 
+
 function App() {
   return (
-    <>
       <Home/>
-    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import App from './App.jsx'
 import './index.css'
+import Register from './pages/Register.jsx'
+import Success from './pages/Success.jsx'
+
+const router = createBrowserRouter([
+  {
+      path: "/",
+      element: <App />,
+},{
+      path: "/success",
+      element: <Success />,
+  },
+  {
+      path:"/register",
+      element: <Register />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
