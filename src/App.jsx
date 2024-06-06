cf20d657606249373fe94ecd20597d0b1a5ef2c4
Mahesh kumar <mahesh.kr.2277@gmail.com>
2024-06-06 14:22:17 +0530

    feat: smooth scrolling using @Locomotive-scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap/all";
+import LocomotiveScroll from 'locomotive-scroll';
 
 import Home from "./pages/Home";
 import "./App.css";
 
 
+
+
 function App() {
+  const locomotiveScroll = new LocomotiveScroll();
+
   useGSAP(() => {
     gsap.set("body", { autoAlpha: 0 });
     gsap.to("body", { autoAlpha: 1, duration: 1 });
